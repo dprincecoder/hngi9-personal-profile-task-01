8e5eb4651e1833d256601a45e4a6b7053cb28f27
dprincecoder <dprincecoder@gmail.com>
2022-10-23 23:41:39 +0100

    add additional links

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,25 @@ function App() {
       <div className="container">
         <Header />
         <Button>Twitter Link</Button>
+        <Button id="btn__zuri" link="https://training.zuri.team/">
+          Zuri Team
+        </Button>
+        <Button id="books" link="http://books.zuri.team/">
+          Zuri Books
+        </Button>
+
+        <Button
+          id="book__python"
+          link="https://books.zuri.team/python-for-beginers?ref_id=dprincecoder"
+        >
+          Python Books
+        </Button>
+        <Button id="pitch" link="https://background.zuri.team/">
+          Background Check for Coders
+        </Button>
+        <Button id="book__design" link="https://books.zuri.team/design-rules">
+          Design Books
+        </Button>
       </div>
     </div>
   );
