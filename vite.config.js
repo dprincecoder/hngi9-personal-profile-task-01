ba091f4c1e07329d45b48094b8479ceeebcdd86f
dprincecoder <dprincecoder@gmail.com>
2022-10-27 00:21:33 +0100

    fixes

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  base: "/hngi9-personal-profile-task-01/",
+  plugins: [react()],
+});
